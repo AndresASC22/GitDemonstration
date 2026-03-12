91c066455ce05472eb802618214e6f779c2cffa1
Amadou Barrie <amadoubarrie@Amadous-MacBook-Air-2.local>
2026-03-12 15:26:28 -0400

    new commit

--- script.py
+++ script.py
@@ -1,12 +1,13 @@
 class Foo:
-    def __init__(self, name, color):
+    def __init__(self, name, color, size):
         self.name = name
         self.color = color
+        self.size = size
     
     def printFoo(self):
-        print("Foo name is " + self.name + " foo color is " + self.color)
+        print("Foo name is " + self.name + " foo color is " + self.color + " foo size is " + self.size)
 
-obj = Foo("Philip", "Blue")
+obj = Foo("Philip", "Blue", "large")
 obj.printFoo()
 
 print("Hello World")

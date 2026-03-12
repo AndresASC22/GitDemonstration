dbddcd31265a19d21769195d33530e3a529f875d
Andres. R <amromero2005@gmail.com>
2026-03-12 15:39:20 -0400

    Gave the foo an age

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
 class Foo:
-    def __init__(self, name, color, size):
+    def __init__(self, name, color, size, age):
         self.name = name
         self.color = color
         self.size = size
+        self.age = age
     
     def printFoo(self):
-        print("Foo name is " + self.name + " foo color is " + self.color + " foo size is " + self.size)
+        print("Foo name is " + self.name + " foo color is " + self.color + " foo size is " + self.size +
+                " and age is " + self.age)
 
-obj = Foo("Philip", "Blue", "large")
+obj = Foo("Philip", "Blue", "large", "20")
 obj.printFoo()
 
 print("Hello World")
